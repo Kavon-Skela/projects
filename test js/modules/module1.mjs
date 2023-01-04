fe95aa92348278d48232e668c5efbe958e5edd46
Aleksandr <alexnovaklawyer@gmail.com>
2023-01-04 16:33:35 +0200

    Initial commit

---
+++ test js/modules/module1.mjs
@@ -0,0 +1,3 @@
+let sum = (a, b) => a + b;
+let pow = (a, b) => a ** b;
+export { sum, pow}
---
+++ test js/modules/module2.mjs
@@ -0,0 +1,3 @@
+import { sum, pow as how } from './module1.mjs';
+console.log(sum(2, 3))
+console.log(how(2, 3))
